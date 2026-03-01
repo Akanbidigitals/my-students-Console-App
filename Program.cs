af68e3aea6d3f453711db36622a2a4de78821f82
Munirudeen Akanbi <akanbimunirudeen@gmail.com>
2026-03-01 09:09:58 +0100

    my students first console app

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//Console.WriteLine("Hello world");
+//Console.WriteLine("my name is Munirudeen");
+
+//Variable Declearation(int , double , string,  bool)
+
+int age = 50;
+
+string name = "Munirudeen";
+
+double height = 34.5;
+
+bool isPresent =  false;
+
+
+////////////////////////////////////////
+
+//Console.WriteLine(name + " " + height); // string concatination
+
+//Console.WriteLine($"{name} {height}"); // string interpolation
+
+// for string interpolation
+//Console.WriteLine($"my name is {name}, i am {age} years old. My heigh is {height}. are u a christain:{isPresent}");
+
+// for Concatination
+Console.WriteLine("my name is " + name + ", i am " + age + " years old. my height is " + height + ".are u a christain:" + isPresent);
+
+
+
+
+
+
+
